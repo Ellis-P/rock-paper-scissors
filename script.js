c180986b44a9bcf053c469c5b48392480b71c2ea
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-01 18:22:14 +0200

    Create getRandomNum and generateChoice functions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+/* 
+getComputerChoice logic:
+Goal - getComputerChoice randomly returns one of rock/paper/scissors
+1. Generate a random number between 0 and 2 inclusive
+2. Return rock, paper or scissors based on the number generated (e.g. if 1, return rock, 2 paper etc.)
+*/
+
+// Generates a random number between 0 and 2
+function getRandomNum() {
+  let randomNum = Math.floor(Math.random() * 3);
+  return randomNum;
+}
+
+// 
+function generateChoice(num) {
+  if (num === 0) {
+    return "Rock";
+  } else if (num === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+function getComputerChoice() {
+
+}
