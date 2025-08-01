791a4364668cbeab5b3b3577d256f0cb4fb6ebfe
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-01 19:34:54 +0200

    Create getHumanChoice function

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
+
+/* getHumanChoice logic:
+prompt the user for input, converting to lower case and storing the result in a variable
+check the response variable to see if it matches one of rock, paper or scissors
+return the matching choice
+*/
+
+function getHumanChoice() {
+  const playerInput = prompt("Your move! Select rock, paper or scissors.").toLowerCase();
+
+  if (playerInput === "rock") {
+    return "rock";
+  } else if (playerInput === "paper") {
+    return "paper";  
+  } else if (playerInput === "scissors") {
+    return "scissors";
+  }
+}
+
+getHumanChoice();
