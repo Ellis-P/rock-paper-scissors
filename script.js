c14006a766fa3ecba3244a9e719239ebd0c34ac7
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-08 10:15:18 +0200

    Add playerSelection buttons and event listeners to play a single round

--- script.js
+++ script.js
@@ -17,58 +17,56 @@ function getComputerChoice() {
   }
 }
 
-// Prompts user for input and converts it to lower case. NOTE: Does not handle invalid inputs yet!
-function getHumanChoice() {
-  const playerInput = prompt("Your move! Select rock, paper or scissors.").toLowerCase();
-
-  if (playerInput === "rock") {
-    return "rock";
-  } else if (playerInput === "paper") {
-    return "paper";  
-  } else if (playerInput === "scissors") {
-    return "scissors";
-  }
-}
-
-// Full game loop, tracks scores and runs the playRound function five times. After five rounds, declares the result (win, lose or draw).
-function playGame () {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  // Play a single round
-  function playRound() {
-    const humanChoice = getHumanChoice();
+// Play a single round
+  function playRound(playerSelection) {
     const computerChoice = getComputerChoice();
     
-    if (humanChoice === computerChoice) {
+    if (playerSelection === computerChoice) {
       console.log("It's a draw!");
     } else if (
-      humanChoice === "rock" && computerChoice === "scissors" ||
-      humanChoice === "paper" && computerChoice === "rock" ||
-      humanChoice === "scissors" && computerChoice === "paper"
+      playerSelection === "rock" && computerChoice === "scissors" ||
+      playerSelection === "paper" && computerChoice === "rock" ||
+      playerSelection === "scissors" && computerChoice === "paper"
     ) {
-      console.log(`GG! You won this round, ${humanChoice} beats ${computerChoice}.`);
-      humanScore++;
+      console.log(`GG! You won this round, ${playerSelection} beats ${computerChoice}.`);
+      // humanScore++;
     } else {
-      console.log(`Unlucky! You lost this round, ${computerChoice} beats ${humanChoice}.`);
-      computerScore++;
+      console.log(`Unlucky! You lost this round, ${computerChoice} beats ${playerSelection}.`);
+      // computerScore++;
     }
-    console.log(`The score is: You - ${humanScore} | Computer - ${computerScore}`);
+    // console.log(`The score is: You - ${humanScore} | Computer - ${computerScore}`);
   }
 
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-  playRound();
+const buttons = document.querySelectorAll(".button");
 
-  if (humanScore === computerScore) {
-    console.log(`Aaaand it's a draw, final score: You - ${humanScore} | Computer - ${computerScore}`);
-  } else if (humanScore > computerScore) {
-    console.log(`Congratulations, you won the game! Final score: You - ${humanScore} | Computer - ${computerScore}`);
-  } else {
-    console.log(`You lost! Better luck next time. Final score: You - ${humanScore} | Computer - ${computerScore}`);
-  }
-}
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const playerSelection = event.target.textContent.toLowerCase();
+    playRound(playerSelection);
+  });
+});
 
-playGame();
+// Full game loop, tracks scores and runs the playRound function five times. After five rounds, declares the result (win, lose or draw).
+// function playGame () {
+//   let humanScore = 0;
+//   let computerScore = 0;
+
+//   playRound();
+//   playRound();
+//   playRound();
+//   playRound();
+//   playRound();
+
+//   if (humanScore === computerScore) {
+//     console.log(`Aaaand it's a draw, final score: You - ${humanScore} | Computer - ${computerScore}`);
+//   } else if (humanScore > computerScore) {
+//     console.log(`Congratulations, you won the game! Final score: You - ${humanScore} | Computer - ${computerScore}`);
+//   } else {
+//     console.log(`You lost! Better luck next time. Final score: You - ${humanScore} | Computer - ${computerScore}`);
+//   }
+// }
+
+// playGame();
+
+
+
