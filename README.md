ab43b2093e39bcb382347ec72bcaca1dd5254d4e
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-10 13:46:42 +0200

    Implement full game cycle functionality, and event listener removal

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ The Odin Project - Rock Paper Scissors (Foundations)
 Add a div for displaying results and change all of your console.logs into DOM methods.
 Display the running score, and announce a winner of the game once one player reaches 5 points.
 You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life. -->
+
--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function getRandomNum() {
   let randomNum = Math.floor(Math.random() * 3);
   return randomNum;
 }
-
 // Evaluates the random number generated to determine which move to make
 function getComputerChoice() {
   const num = getRandomNum();
@@ -16,53 +15,61 @@ function getComputerChoice() {
     return "scissors";
   }
 }
-
 // Play a single round
 function playRound(playerSelection) {
   const computerChoice = getComputerChoice();
 
   if (playerSelection === computerChoice) {
-    result.textContent = "It's a draw!";
+    roundResult.textContent = "It's a draw!";
   } else if (
     playerSelection === "rock" && computerChoice === "scissors" ||
     playerSelection === "paper" && computerChoice === "rock" ||
     playerSelection === "scissors" && computerChoice === "paper"
   ) {
-    result.textContent = `GG! You won this round, ${playerSelection} beats ${computerChoice}.`;
+    roundResult.textContent = `GG! You won this round, ${playerSelection} beats ${computerChoice}.`;
     humanScore++;
   } else {
-    result.textContent = `Unlucky! You lost this round, ${computerChoice} beats ${playerSelection}.`;
+    roundResult.textContent = `Unlucky! You lost this round, ${computerChoice} beats ${playerSelection}.`;
     computerScore++;
   }
 
   gameResult();
 }
-
-// Full game loop, tracks scores and runs the playRound function five times. After five rounds, declares the result (win, lose or draw).
+// Check result and remove event handlers based on score, to avoid going above 5 points
 function gameResult () {
-  if (humanScore === 5) {
-    result.textContent = `Congratulations, you won the game! Final score: You - ${humanScore} | Computer - ${computerScore}`;
-  } else if (computerScore === 5) {
-    result.textContent = `You lost! Better luck next time. Final score: You - ${humanScore} | Computer - ${computerScore}`;
-  } else {
-    result.textContent = `The score is: You - ${humanScore} | Computer - ${computerScore}`;
+  if (!(humanScore === 5 || computerScore === 5)) {
+    currentScore.textContent = `The score is: You - ${humanScore} | Computer - ${computerScore}`;
+    return;
   }
-}
+  
+  if (humanScore === 5) {
+    currentScore.textContent = `Congratulations, you won the game! Final score: You - ${humanScore} | Computer - ${computerScore}`;
+  }
 
-// Button click event listener
-const buttons = document.querySelectorAll(".button");
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    const playerSelection = event.target.textContent.toLowerCase();
-    playRound(playerSelection);
+  if (computerScore === 5) {
+    currentScore.textContent = `You lost! Better luck next time. Final score: You - ${humanScore} | Computer - ${computerScore}`;
+  }
+  // Remove event listener once games ends (player or computer has 5 points)
+  buttons.forEach((btn) => {
+    btn.removeEventListener("click", handleClick)
   });
-});
-
-const gameInfo = document.querySelector(".gameInfo");
-const result = document.createElement("p");
-gameInfo.appendChild(result);
+}
+// Click handler function, necessary for easy listener removal
+function handleClick(event) {
+  const playerSelection = event.target.textContent.toLowerCase();
+  playRound(playerSelection);
+}
+// Variables
+const roundResult = document.querySelector(".round-result");
+const currentScore = document.querySelector(".current-score");
+const buttons = document.querySelectorAll(".button");
+const gameInfo = document.querySelector(".game-info");
 let humanScore = 0;
 let computerScore = 0;
+// Add event listeners to all buttons
+buttons.forEach((btn) => {
+  btn.addEventListener("click", handleClick);
+});
 
 
 
